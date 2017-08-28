6629e892ac4fda39ec73bccca0712eac9eeb73b3
Kelvin Zhang <zhangxp1998@gmail.com>
2017-08-28 11:11:21 -0400

    Add runtime growth analysis code

---
+++ src/PercolationAnalysis.java
@@ -0,0 +1,23 @@
+
+public class PercolationAnalysis
+{
+	private static final int[] sizes = { 10, 25, 50, 100, 250, 500 };
+
+	public static void main(String[] args)
+	{
+		System.out.println("========================fast========================");
+		for (int n : sizes)
+		{
+			PercolationStats ps = new PercolationStats(n, 30, PercolationStats::fast);
+			System.out.println(n + " : " + ps.meanTime());
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println("========================slow========================");
+		for (int n : sizes)
+		{
+			PercolationStats ps = new PercolationStats(n, 30, PercolationStats::slow);
+			System.out.println(n + " : " + ps.meanTime());
+		}
+	}
+}
